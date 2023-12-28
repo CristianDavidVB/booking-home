e9d76f89a50aa7a7190ddda1bd006725a61a63e1
Cristian Villanova <cristiandavidv@hotmail.com>
2023-12-28 16:11:15 -0500

    feat(community-feature): add import logic from CSV in seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,15 @@
-# This file should ensure the existence of records required to run the application in every environment (production,
-# development, test). The code here should be idempotent so that it can be executed at any point in every environment.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Example:
-#
-#   ["Action", "Comedy", "Drama", "Horror"].each do |genre_name|
-#     MovieGenre.find_or_create_by!(name: genre_name)
-#   end
+require 'csv'
+csv_file_path = Rails.root.join('db', 'data', 'comunas.csv')
+
+if File.exist?(csv_file_path)
+  puts 'Importing communities...'
+
+  csv_data = CSV.read(csv_file_path, headers: true, encoding: 'utf-8')
+  csv_data.each do |row|
+    Community.create!(id: row['id'], name: row['name'])
+  end
+
+  puts 'Communities imported!'
+else
+  puts 'Communities not imported!'
+end
