1685920c5f97e0b097bd507e3c08f84c7dff7792
Cristian Villanova <cristiandavidv@hotmail.com>
2023-12-28 21:35:45 -0500

    feat(property-feature): add helper files and views for property management

---
+++ app/helpers/properties_helper.rb
@@ -0,0 +1,13 @@
+module PropertiesHelper
+  def property_types_options
+    Property.property_types.keys.map do |property_type|
+      [property_type.humanize, property_type]
+    end
+  end
+
+  def currencies_options
+    Property.currencies.keys.map do |currency|
+      [currency.humanize, currency]
+    end
+  end
+end
