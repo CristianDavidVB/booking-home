c0021fcce117924d4bb41ec598ec83b69538d181
Cristian Villanova <cristiandavidv@hotmail.com>
2023-12-30 15:19:32 -0500

    feat(feature/consume-currency-api): add fetch currency api service file

---
+++ app/services/fetch_currency_api_service.rb
@@ -0,0 +1,34 @@
+class FetchCurrencyApiService
+  BASE_URL = 'https://mindicador.cl/api/dolar'.freeze
+
+  def call
+    fetch_currency_api || 0
+  rescue Faraday::ConnectionFailed => e
+    Rails.logger.info "Error connecting to API: #{e.message}"
+    []
+  end
+
+  private
+
+  def fetch_currency_api
+    usd_value = parse_response(api_request("#{BASE_URL}/#{current_date}"))
+
+    usd_value.blank? ? parse_response(api_request("#{BASE_URL}/#{previous_date}")) : usd_value
+  end
+
+  def api_request(url)
+    Faraday.get(url)
+  end
+
+  def current_date
+    Time.current.strftime('%d-%m-%Y')
+  end
+
+  def previous_date
+    (Date.current - 1.day).strftime('%d-%m-%Y')
+  end
+
+  def parse_response(response)
+    JSON.parse(response.body)['serie']&.first&.[]('valor')
+  end
+end
