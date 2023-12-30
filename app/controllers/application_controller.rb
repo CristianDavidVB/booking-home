bf86c54c4ba9b46c72dce5356053bf37357d23d7
Cristian Villanova <cristiandavidv@hotmail.com>
2023-12-30 15:42:45 -0500

    refactor(feature/user): update controller and views for the user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   include Pagy::Backend
+
+  def after_sign_in_path_for(resource)
+    if resource.is_a?(User)
+      user_properties_path
+    else
+      super
+    end
+  end
 end
